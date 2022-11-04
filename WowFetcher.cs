069f12b0dc5664bc8f42735b8a3449228205df16
Jason Chesters-salt <43147625+jasonTheHuman2101@users.noreply.github.com>
2022-11-04 14:54:11 +0000

    Add project files.

---
+++ WowFetcher.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WOWBot
+{
+    public static class WowFetcher
+    {
+        public static Wow GetWow()
+        {
+            Wow newWow = new Wow();
+            return newWow;
+        }
+    }
+
+    public class Wow
+    {
+
+    }
+}
